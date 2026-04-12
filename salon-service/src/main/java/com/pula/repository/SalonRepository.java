63d18d6e3078e41712994daa32fe9426370aa3a8
pula-ashok <ashokpula0307@gmail.com>
2026-04-12 16:53:03 +0530

    updated repository for search salons functionality

--- salon-service/src/main/java/com/pula/repository/SalonRepository.java
+++ salon-service/src/main/java/com/pula/repository/SalonRepository.java
@@ -2,8 +2,25 @@ package com.pula.repository;
 
 import com.pula.model.Salon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SalonRepository extends JpaRepository<Salon,Long> {
+    Salon findByOwnerId(Long id);
+
+    @Query(
+            "select s from Salon s where "+
+                    "(lower(s.city) like lower(concat('%',:keyword,'%')) OR "+
+                    "lower(s.name) like lower(concat('%',:keyword,'%')) OR "+
+                    "lower(s.address) like lower(concat('%',:keyword,'%')))"
+    )
+//    @Query("SELECT s FROM Salon s WHERE " +
+//        "(LOWER(s.city) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+//        "LOWER(s.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+//        "LOWER(s.address) LIKE LOWER(CONCAT('%', :keyword, '%')))")
+    List<Salon> searchSalons(@Param("keyword") String keyword);
 }
